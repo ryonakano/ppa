9811af6f31371576f8ef1504e74be0e8a3f6b607
Ryo Nakano <26003928+ryonakano@users.noreply.github.com>
2022-10-16 13:13:06 +0900

    install: Fix PPA not installed actually

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -eu
 
-sudo apt install -y wget
-sudo wget https://raw.githubusercontent.com/ryonakano/ppa/main/ryonakano-patch.pref -O /etc/apt/preferences.d/ryonakano-patches.pref
+# prerequirements
+sudo apt install -y curl
+
+# install priority file
+sudo curl -o /etc/apt/preferences.d/ryonakano-patches.pref "https://ryonakano.github.io/ppa/ryonakano-patch.pref"
+
+# install PPA
+curl -s --compressed "https://ryonakano.github.io/ppa/debian/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/ryonakano_ppa.gpg >/dev/null
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/ryonakano_ppa.gpg] https://ryonakano.github.io/ppa/debian ./" | sudo tee /etc/apt/sources.list.d/ryonakano_ppa.list
+
+# upgrade only the target package
 sudo apt update
 sudo apt install base-files
